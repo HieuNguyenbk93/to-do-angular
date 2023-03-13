08e5bc627aa90b59b24af80eb0f71a5425c87808
Hieu Nguyen <hieunguyenbk93@gmail.com>
2023-03-13 16:11:18 +0700

    finish component

--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { Filter } from '../models/filtering.model';
+import { Todo } from '../models/todo.model';
+import { LocalStorageService } from './local-storage.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TodoService {
 
-  constructor() { }
+  private static readonly TodoStorageKey = 'todos';
+
+  private todos: Todo[] = [];
+  private filteredTodos: Todo[] = [];
+  private lengthSubject: BehaviorSubject<number> = new BehaviorSubject<number>(0);
+  private displayTodosSubject: BehaviorSubject<Todo[]> = new BehaviorSubject<Todo[]>([]);
+  private currentFilter: Filter = Filter.All;
+
+  todo$: Observable<Todo[]> = this.displayTodosSubject.asObservable();
+  length$: Observable<number> = this.lengthSubject.asObservable();
+
+  constructor(
+    private storageService: LocalStorageService
+  ) { }
+
+
 }
